adaf75e048bceaca47104347c998bce95c4f15de
mkandan <40218068+mkandan@users.noreply.github.com>
2018-07-30 04:03:53 +0900

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1vtIhGAmdVBCyNO4Q7U7Cs3zlkJA15U3m-T16EetYzy8/edit#gid=0';
